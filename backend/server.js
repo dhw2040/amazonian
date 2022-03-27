9ca596b4f38efceb0e14ab272727810531c98e1a
dhw2040 <dhw2040@github.com>
2022-03-27 00:13:27 -0400

    signin in the userRouter added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
 
 const port = process.env.port || 5000; // env var
+dotenv.config(); // use .env file
 
 const app = express();
 
@@ -15,23 +17,10 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-// app.get("/api/products/:id", (req, res) => {
-//   const product = data.products.find((x) => x._id === req.params.id);
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: "Product not found" });
-//   }
-// });
-
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// }); Static data
-
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 
-// Middleware that handles
+// Middleware that handles error
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
